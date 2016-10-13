7b60f67403ba9d17662766d652f8651bb1e68048
Dominic Martinez <martinezd19@trinityprep.org>
2016-10-13 08:55:07 -0400

    Finish detective?
    Finish detective, BFS, need to look at input specs.

--- src/detective.java
+++ src/detective.java
@@ -7,16 +7,50 @@ public class detective {
         for(int i = 0; i < cases; i++) {
             int lines = Integer.parseInt(in.nextLine());
             HashMap<String, String[]> map = new HashMap<>();
+            HashMap<String, Boolean> mapVisited = new HashMap<>();
             for(int x = 0; x < lines; x++) {
                 String[] names = in.nextLine().split(" ");
                 String[] suspects = {names[1], names[2]};
                 map.put(names[0], suspects);
+                mapVisited.put(names[0], false);
             }
             boolean done = false;
             String[] suspects;
-            for(int x = 0; x < lines; x++) {
-                
+            String witness;
+            if(map.get("Margarita")[0].equals("James")) {
+                witness = map.get("Margarita")[1];
+            } else {
+                witness = map.get("Margarita")[0];
             }
+            Queue<String> queue = new LinkedList<>();
+            HashMap<String, String> path = new HashMap<>();
+            queue.add(witness);
+            mapVisited.put(witness, true);
+            mapVisited.put("Margarita", true);
+            path.put(witness, "Margarita");
+            int length = -1;
+            while(!queue.isEmpty()) {
+                witness = queue.poll();
+                suspects = map.get(witness);
+                if(suspects[0].equals("James") || suspects[1].equals("James")) {
+                    length = 0;
+                    while(true) {
+                        witness = path.get(witness);
+                        length++;
+                        if(witness.equals("Margarita")) {
+                            break;
+                        }
+                    }
+                    break;
+                }
+                for(int x = 0; x < 2; x++) {
+                    if(!mapVisited.get(map.get(witness)[i])) {
+                        queue.add(map.get(witness)[i]);
+                        path.put(map.get(witness)[i], witness);
+                    }
+                }
+            }
+            System.out.println(length);
         }
     }
 }
